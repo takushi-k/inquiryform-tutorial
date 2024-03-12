b33947f6c6da3fe85f559c69d6df6b0ece6d7596
takushi <takutakushii@gmail.com>
2024-03-12 18:47:42 +0900

    my-first-commit

---
+++ components/MailForm/MailForm.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import React, { useEffect } from 'react'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Textarea } from '../ui/textarea'
+import { useMailForm } from '@/hooks/useMailForm'
+import { ClipLoader } from "react-spinners";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const MailForm = () => {
+  console.log("MailForm!!!!!!!")
+
+  const {form, onSubmit} = useMailForm()
+
+  useEffect(() => {
+    if(form.formState.isSubmitSuccessful){
+      toast.success("メール送信に成功しました！")
+    }
+  }, [form.formState.isSubmitSuccessful])
+
+  return (
+    <Form {...form}>
+      <ToastContainer />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="container flex flex-col gap-3">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ユーザー名</FormLabel>
+              <FormControl>
+                <Input placeholder="ユーザー名" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input placeholder="メールアドレス" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>主題</FormLabel>
+              <FormControl>
+                <Input placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea placeholder="本文" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field: { value, onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel>添付画像</FormLabel>
+              <FormControl>
+                <Input
+                  accept="image/*"
+                  type="file"
+                  {...fieldProps}
+                  onChange={(event) => {
+                    onChange(event.target.files)
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+
+        <Button type="submit" disabled={form.formState.isSubmitting} >
+          {form.formState.isSubmitting ? <ClipLoader /> : "送信"}
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+export default MailForm
---
+++ hooks/useMailForm.ts
@@ -0,0 +1,47 @@
+import { formSchema } from "@/lib/formSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+export const useMailForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      subject: "",
+      email: "",
+      content: "",
+      file: undefined,
+    },
+  });
+
+  const onSubmit = useCallback(async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    const {username, email, subject, content, file} = values
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("username", username)
+    formData.append("email", email)
+    formData.append("subject", subject)
+    formData.append("content", content)
+    formData.append("file", file[0])
+
+    try{
+      await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/send`, {
+        method: "POST",
+        body: formData
+      })
+    }catch(err){
+      console.log(err)
+    }
+
+
+  }, [])
+
+
+
+  return {form, onSubmit}
+}
+
